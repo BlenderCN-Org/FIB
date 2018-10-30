1ba84a9da0b9497a990e9d371011fcfafc32bbbb
Emeline <got.emeline@gmail.com>
2018-10-30 12:48:22 +0100

    Update VAR

---
+++ VAR/ProjectAR/Unity/AR_Project/Assets/Scripts/VBScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class VBScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject VBButtonObject;
+	private GameObject Cat;
+
+	// Use this for initialization
+	void Start () {
+
+		VBButtonObject = GameObject.Find ("actionbutton");
+		VBButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+		Cat = GameObject.Find ("Cat");
+
+	}
+	
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+	
+		Debug.Log ("pressed");
+		//Cat.transform.Rotate (new Vector3 (0, Time.deltaTime * 1000, 0));
+		Cat.GetComponent<Animation> ().Play (); 
+
+	}
+
+
+	public void OnButtonReleased( VirtualButtonAbstractBehaviour vb ) { 
+
+		Debug.Log ("unpressed");
+		Cat.GetComponent<Animation> ().Stop (); 
+
+	}
+
+}
---
+++ VAR/ProjectAR/Unity/AR_Project/Assets/Scripts/VBScript2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class VBScript2 : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject VBButtonObject;
+	private GameObject Tigre;
+
+	// Use this for initialization
+	void Start () {
+
+		VBButtonObject = GameObject.Find ("rotatebutton");
+		VBButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+		Tigre = GameObject.Find ("Tigre");
+	}
+
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+
+		Debug.Log ("pressed");
+		Tigre.transform.Rotate(new Vector3 (0, Time.deltaTime * 2000, 0));
+
+	}
+
+
+	public void OnButtonReleased( VirtualButtonAbstractBehaviour vb ) { 
+
+		Debug.Log ("unpressed"); 
+
+	}
+
+}
